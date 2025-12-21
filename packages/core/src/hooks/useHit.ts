807d13328bf6d2d997586696766ba9bd407aa0d1
Pinia1 <574977706@qq.com>
2025-12-22 00:26:29 +0800

    fix:hit listing

--- packages/core/src/hooks/useHit.ts
+++ packages/core/src/hooks/useHit.ts
@@ -1,13 +1,18 @@
 import { useMemoizedFn } from '@zeroDraw/common';
 import { useRef, useState } from 'react';
+import { useShallow } from 'zustand/react/shallow';
 import useHitStore from '../store/useHit';
+import useToolsStore from '../store/useTools';
+import { Actions } from '../types/Drawing';
 
 const useHit = ({ opacity, id }: { opacity: number; id: string }) => {
   const removeTagRef = useRef(false);
   const [shapeOpacity, setShapeOpacity] = useState(opacity || 1);
+  const activeKey = useToolsStore(useShallow((s) => s.activeKey));
 
   const handleMouseEnter = useMemoizedFn(() => {
     if (removeTagRef.current) return;
+    if (activeKey !== Actions.REMOVE) return;
 
     const { isHit, hitIds } = useHitStore.getState();
     if (!isHit) return;
